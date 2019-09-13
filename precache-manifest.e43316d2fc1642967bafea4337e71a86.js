f5d35604cd4a0a3690caaa1c6ac0b6b2a46f6960
Jehad AbuDabbous <Jehad.AbuDabbous@Realsoft-me.com>
2019-09-13 16:17:20 +0300

    Updates

---
+++ precache-manifest.e43316d2fc1642967bafea4337e71a86.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a63fa41bf6b69871d2278e261302dec",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "7d776096cc24b6ec191f",
+    "url": "/monster-rolodex/static/css/main.d3ddfe84.chunk.css"
+  },
+  {
+    "revision": "9e1e22d61619314af4f5",
+    "url": "/monster-rolodex/static/js/2.731441e9.chunk.js"
+  },
+  {
+    "revision": "7d776096cc24b6ec191f",
+    "url": "/monster-rolodex/static/js/main.a3431a65.chunk.js"
+  },
+  {
+    "revision": "df639e06247d3898a521",
+    "url": "/monster-rolodex/static/js/runtime~main.bebe8c9b.js"
+  }
+]);
